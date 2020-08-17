121a4e882b5a6a59545a0a62c69c3285e690279e
Peleg Sapir <peleg@ds.mpg.de>
2020-08-17 13:46:42 +0200

    WIP: cython conversion

--- cython/lib.pyx
+++ cython/lib.pyx
@@ -19,12 +19,26 @@ cdef class vector3:
         self.x = x
         self.y = y
         self.z = z
+    
+    def __add__(self, v):
+        return self.add(v)
+
+    def __str__(self):
+        return '({},{},{})'.format(self.x,
+                                   self.y,
+                                   self.z)
+
+    cpdef vector3 add(self, vector3 v):
+        return vector3(self.x+v.x,
+                       self.y+v.y,
+                       self.z+v.z)
 
     cpdef double c_dot(self, v):
         return self.x*v.x + self.y*v.y + self.z*v.z
-
+    """
     dpdef double c_norm(self):
         return sqrt(self.c_dot(self))
 
     dpdef double c_squrenorm(self):
         return self.c_dot(self)
+    """
